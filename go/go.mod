04cc21de25e3f3c2ed754a53795939b2680033df
Huakun Shen <huakun.shen@huakunshen.com>
2026-07-20 06:37:18 +0800

    chore: remove go impl and broken ts impl ahead of rewrite

--- go/go.mod
+++
@@ -1,3 +0,0 @@
-module github.com/HuakunShen/netsu/go
-
-go 1.23.0
--- packages/netsu/tsup.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "tsup";
-
-export default defineConfig({
-  entry: ["./src/speed-test.ts"],
-  dts: true,
-  format: ["cjs", "esm"],
-  clean: true,
-});
